1205eb0b23b0cbcdd79ce27ef8f9746a4b8b3727
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-05 18:41:24 +0800

    added IDs to the form

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -5,6 +5,7 @@ import firebaseInitialization from './FirebaseConfig.jsx';
 
 
 function EmployeeList() {
+  
   // Initialize Cloud Firestore and get a reference to the service
   // const db = getFirestore(firebaseInitalization);
 
@@ -16,19 +17,19 @@ function EmployeeList() {
       <div className="border p-3 bg-transparent">
         <div className="row">
           <div className="col-md-4">
-            <input className='form-control' type="text" placeholder='First Name' />
+            <input className='form-control' id='firstname' type="text" placeholder='First Name' />
           </div>
 
           <div className="col-md-4">
-            <input className='form-control' type="text" placeholder='Last Name' />
+            <input className='form-control' id='lastname' type="text" placeholder='Last Name' />
           </div>
 
           <div className="col-md-2">
-            <input className='form-control' type="text" placeholder='Position' />
+            <input className='form-control' id='position' type="text" placeholder='Position' />
           </div>
 
           <div className="col-md-2">
-            <input className='form-control' type="number" placeholder='Salary' />
+            <input className='form-control' id='salary' type="number" placeholder='Salary' />
           </div>
 
           <div className="d-grid gap-2 mt-3">
