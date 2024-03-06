ca064a4b3d5832bacf119ec9d9be7b1cae64d986
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-06 13:12:05 +0800

    added another alert

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -9,9 +9,17 @@ function EmployeeList() {
   return (
     <>
     <h1 className='fw-light text-center'>Employee List</h1>
-    <div class="alert alert-info mt-3" role="alert">
-        FirstName LastName Position Salary
+    <div className="row">
+      <div className="col-md-8 mx-auto">
+        <div class="alert alert-info mt-3" role="alert">
+          FirstName LastName Position Salary
+        </div>
+        <div class="alert alert-info mt-3" role="alert">
+          FirstName LastName Position Salary
+        </div>
+      </div>
     </div>
+    
     </>
   )
  
