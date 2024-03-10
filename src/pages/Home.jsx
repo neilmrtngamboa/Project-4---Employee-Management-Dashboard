535b9b503177a48040b542f7d57ac4f707486c26
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-10 20:27:39 +0800

    added content for the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@
 function Home () {
     return (
         <>
-            <h1>This is the home page</h1>
+            <h1 className="display-1"> EMD</h1>
+            <small className="fs-5">A Employee Management Dashboard Web Application that can Create, Read, Update and Delete (CRUD) records.</small>
         </>
     )
 }
