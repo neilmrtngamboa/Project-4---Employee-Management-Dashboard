9f31cd2f1a6aa34ebfce2523574e5fbf9cbd4973
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-13 17:33:08 +0800

    created forms and a button

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -4,10 +4,35 @@ import FirebaseInitialization from '../FirebaseConfig';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 function Register () {
+    
     return (
-        <>
-        <h1>Register Page</h1>
-        </>
+        <section>
+        <div className="container-fluid bg-light p-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <label htmlFor="registerFirstName" className="text-dark mb-2">First Name:</label>
+                    <input type="text" placeholder="First Name" id="registerFirstName" className="form-control mb-2" />
+                </div>
+
+                <div className="col-md-6">
+                    <label htmlFor="registerLastName" className="text-dark mb-2">Last Name:</label>
+                    <input type="text" placeholder="Last Name" id="registerLastName" className="form-control" />
+                </div>
+            </div>
+
+            <label htmlFor="registerEmail" className="text-dark mb-2">Email:</label>
+            <input type="email" placeholder="yourname@sample.com" id="registerEmail" className="form-control" />
+
+            <label htmlFor="registerPassword" className="text-dark mt-2">Password:</label>
+            <input type="Password" id="registerPassword" className="form-control mb-3" />
+
+            <button className="btn btn-primary mb-4">Create an Account</button>
+
+            <p className="text-dark">Already have an account? <Link to='login'>Login here</Link></p>
+
+
+        </div>
+        </section>
     )
 }
 export default Register;
