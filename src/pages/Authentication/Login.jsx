5808a3c1154cb62cb1324b8a30b5e9d46054242b
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-13 17:08:52 +0800

    added forms to login

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
 
 function Login () {
     return (
         <>
-        <h1>Login Page</h1>
+        <div className="container-fluid bg-primary p-3 my-auto">
+            <h1 className="m-3">Login</h1>
+            <input type="email" className="form-control mb-3" placeholder="Email" />
+            <input type="password" className="form-control mb-3" placeholder="Password" />
+
+            <button className="btn btn-light">Sign In</button>
+
+            <p className="mt-3">Don't have an account? <Link to='/regiter' className="text-dark">Register here</Link></p>
+        </div>
         </>
     )
 }
