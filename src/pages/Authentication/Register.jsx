3959e4ca904ff381e19f92c083c19e9abcb14f4f
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-13 17:22:44 +0800

    imported

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import FirebaseInitialization from '../FirebaseConfig';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 function Register () {
     return (
