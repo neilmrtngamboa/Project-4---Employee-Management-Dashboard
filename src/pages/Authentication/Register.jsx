242517b4a0bbc15696b94eed869cafe4816240dd
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-13 17:36:34 +0800

    added an the rest of the onchange events

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -25,21 +25,26 @@ function Register () {
 
                 <div className="col-md-6">
                     <label htmlFor="registerLastName" className="text-dark mb-2">Last Name:</label>
-                    <input type="text" placeholder="Last Name" id="registerLastName" className="form-control" />
+                    <input type="text" placeholder="Last Name" id="registerLastName" className="form-control"
+                    onChange={(e) => setUserLastName(e.target.value)} value={userLastName}
+                    />
                 </div>
             </div>
 
             <label htmlFor="registerEmail" className="text-dark mb-2">Email:</label>
-            <input type="email" placeholder="yourname@sample.com" id="registerEmail" className="form-control" />
+            <input type="email" placeholder="yourname@sample.com" id="registerEmail" className="form-control"
+            onChange={(e) => setEmail(e.target.value)} value={email}
+            />
 
             <label htmlFor="registerPassword" className="text-dark mt-2">Password:</label>
-            <input type="Password" id="registerPassword" className="form-control mb-3" />
+            <input type="Password" id="registerPassword" className="form-control mb-3"
+            onChange={(e) => setPassword(e.target.value)} value={password}
+            />
 
             <button className="btn btn-primary mb-4">Create an Account</button>
 
             <p className="text-dark">Already have an account? <Link to='login'>Login here</Link></p>
 
-            <p className="text-dark">{userFirstName}</p>
         </div>
         </section>
     )
