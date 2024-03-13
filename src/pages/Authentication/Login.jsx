c3ed66c8c6930fb76b2e8885b19cf9cab55cfbd8
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-03-13 17:12:43 +0800

    added an onchange event for email & added the getAuth from firebase

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function Login () {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <>
         <div className="container-fluid bg-primary p-3 my-auto">
             <h1 className="m-3">Login</h1>
-            <input type="email" className="form-control mb-3" placeholder="Email" />
+            <input type="email" className="form-control mb-3" placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)} value={email}
+            />
             <input type="password" className="form-control mb-3" placeholder="Password" />
 
             <button className="btn btn-light">Sign In</button>
 
             <p className="mt-3">Don't have an account? <Link to='/regiter' className="text-dark">Register here</Link></p>
+            <p className="text-white">{email}</p>
         </div>
         </>
     )
